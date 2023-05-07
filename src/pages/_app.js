99e4afec370f8c2f0a630598961cfea8948acd72
BogdanGlazkov <bglazkov@i.ua>
2023-05-07 17:45:53 +0300

    Add About page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,23 @@
-import '@/styles/globals.css'
+import Head from "next/head";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import "@/styles/globals.css";
+import "@/styles/layout.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  if (Component.getLayout) {
+    return Component.getLayout(<Component {...pageProps} />);
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Code Evolution</title>
+        <meta name="description" content="Awesome YouTube channel" />
+      </Head>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
---
+++ src/pages/about.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import Footer from "../../components/Footer";
+
+function About() {
+  return (
+    <>
+      <Head>
+        <title>About Code Evolution</title>
+        <meta name="description" content="Free tutorial on Next.js" />
+      </Head>
+      <h1 className="content">About</h1>
+    </>
+  );
+}
+
+export default About;
+
+About.getLayout = function (page) {
+  return (
+    <>
+      {page}
+      <Footer />
+    </>
+  );
+};
